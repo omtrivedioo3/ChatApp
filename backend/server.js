2ee5ace3a47455d05c5ee82b5e9b952be3ca5033
Harshdomadiya <harshdomadiya1014@gmail.com>
2024-03-08 21:30:09 +0530

    final 4

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,10 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes); 
 app.use("/api/users",userRoutes); 
 
-app.use(express.static(path.join(__dirname,"/frontend/list")))
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"frontend","list","index.html"))
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
 })
 
 // app.get('/', (req, res) => {
