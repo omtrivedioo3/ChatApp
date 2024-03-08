d4b6a12d89335df13a4e58eb3e7465ff5553e7d8
Harshdomadiya <harshdomadiya1014@gmail.com>
2024-03-08 20:54:47 +0530

    socket impelemented

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,25 +1,38 @@
 import React from 'react'
+import UseConversation from '../../zustend/UseConverstion'
+import { useSocketContext } from '../../context/SocketConext';
 
-function Conversation() {
+function Conversation({conversation,lastIdx,emoji}) {
+  const {selectedConverstion,setSelectedConverstion}=UseConversation()
+
+  const isSelected = selectedConverstion?._id === conversation._id;
+  const { onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(conversation._id);
+  //console.log("hello",conversation);
   return (
     <div>
-      <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer '>
-        <div className='avatar online'>
+      <div className={`flex gap-2 items-center hover:bg-red-700 rounded p-2 py-1 cursor-pointer
+                      ${isSelected?"bg-red-700":""}
+                      `}
+          onClick={()=>setSelectedConverstion(conversation)}            
+        >
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
             <div className='w-12 rounded-full'>
             <img
-				src='https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png'
+				src={conversation.profilepic}
 				alt='user avatar'
                 />
             </div>
         </div>
         <div className='flex flex-col flex-1'>
             <div className='flex gap-3 justify-between'>
-                <p className='font-bold text-gray-200'>harsh D</p>
-                <span className='text-xl'>im</span>
+                <p className='font-bold text-gray-200'>{conversation.fullname}</p>
+                <span className='text-xl'>{emoji}</span>
             </div>
         </div>
       </div>
-      <div className='divider py-0 my-0 h-1'></div>
+     {!lastIdx && <div className='divider py-0 my-0 h-1'/>}
+
     </div>
   )
 }
