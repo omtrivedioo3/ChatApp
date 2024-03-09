48c5e509f174baefb7b4989bf2af1e8088325629
Harshdomadiya <harshdomadiya1014@gmail.com>
2024-03-09 20:32:50 +0530

    socket url

--- frontend/src/context/SocketConext.jsx
+++ frontend/src/context/SocketConext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authuser) {
-			const socket = io("http://localhost:8000", {
+			const socket = io("https://chat-app-r1fx.onrender.com", {
 				query: {
 					userId: authuser.data.id,
 				},
